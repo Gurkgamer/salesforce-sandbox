2d83fefc7d05a282b3618dad2443b3445396d28d
Gurk <gurkgamer@gmail.com>
2023-02-19 00:07:16 +0100

    feat: el viewer ya ve eventos

--- force-app/main/default/lwc/eventViewer/eventViewer.js
+++ force-app/main/default/lwc/eventViewer/eventViewer.js
@@ -1,114 +1,51 @@
-import { LightningElement } from 'lwc';
-import { subscribe, unsubscribe, onError, setDebugFlag, isEmpEnabled,} from 'lightning/empApi';
+import { LightningElement,track } from 'lwc';
+import { subscribe, unsubscribe} from 'lightning/empApi';
 
-export default class EventViewer extends LightningElement 
+export default class EventViewer extends LightningElement
 {
-    channelName = '/event/Log__e';
-    isSubscribeDisabled = false;
-    isUnsubscribeDisabled = !this.isSubscribeDisabled;
+    sfLogEvent = '/event/Log__e';
+    sfLogEventSubscription = {};
+    lastMessage = 'nada por ahora';
 
-    currentMessage = 'texto';
-
-    get lastMessage()
+    connectedCallback()
     {
-        return this.currentMessage;
+        this.subscribeToLogEvent();
     }
 
-    subscription = {};
-
-    // Tracks changes to channelName text field
-    handleChannelName(event) {
-        this.channelName = event.target.value;
-    }
-
-    // Initializes the component
-    connectedCallback() {
-        // Register error listener
-        this.registerErrorListener();
-    }
-
-    setValue(value)
+    subscribeToLogEvent()
     {
-        this.currentMessage = value;
+        this.sfLogEventSubscription = subscribe(this.sfLogEvent,-1,this.handleEvent.bind(this))
+        .then(response =>
+            {
+                console.log('Successfully subscribed to : ', JSON.stringify(response.channel));
+            });
     }
 
-    // Handles subscribe button click
-    handleSubscribe() {
-        // Callback invoked whenever a new event message is received
-        var b = '';
-        const messageCallback = function (response) {
-            console.log('New message received: ', JSON.stringify(response));
-            console.log('a');
-            try
-            {
-                const a = JSON.stringify(response);
-                b = JSON.parse(a);
-                //this.currentMessage = b.data.payload.Message__c;
-                console.log(b);
-                console.log(this.currentMessage);
-                this.currentMessage = b.data.payload.Message__c;
-                
-                //this.setValue = this.currentMessage;
-            }
-            catch(error)
-            {
-                console.log(error);
-                //console.log(JSON.stringify(error));
-            }
-            // Response contains the payload of the new message received
-        };
+    unsuscribeToLogEvent()
+    {
+        unsubscribe(this.sfLogEventSubscription, response => {
+            console.log('Successfully unsubscribed from : ', JSON.stringify(response.channel));
+            // Handle successful unsubscription
+        }, onError);
+    }
 
-        console.log('asdasd');
+    handleEvent(message)
+    {
         try
         {
-            if(b)
-            {
-                console.log('no es este');
-                console.log(b);
-                this.currentMessage = b.data.payload.Message__c;
-                console.log('v');
-                console.log(this.currentMessage);
-            }
+            console.log('Received message: ', JSON.stringify(message));
+            // Handle the received event message
+            this.lastMessage = message.data.payload.Message__c;
         }
         catch(error)
         {
-            console.log(error);
+            console.log('ERROR: '+ error.message);
         }
-
-        // Invoke subscribe method of empApi. Pass reference to messageCallback
-        subscribe(this.channelName, -1, messageCallback).then((response) => {
-            // Response contains the subscription information on subscribe call
-            console.log(
-                'Subscription request sent to: ',
-                JSON.stringify(response.channel)
-            );
-            this.subscription = response;
-            this.toggleSubscribeButton(true);
-            this.currentMessage = 'conectado';
-        });
     }
 
-    // Handles unsubscribe button click
-    handleUnsubscribe() {
-        this.toggleSubscribeButton(false);
-
-        // Invoke unsubscribe method of empApi
-        unsubscribe(this.subscription, (response) => {
-            console.log('unsubscribe() response: ', JSON.stringify(response));
-            // Response is true for successful unsubscribe
-        });
+    refreshComponent()
+    {
+        this.dispatchEvent(new RefreshEvent());
     }
 
-    toggleSubscribeButton(enableSubscribe) {
-        this.isSubscribeDisabled = enableSubscribe;
-        this.isUnsubscribeDisabled = !enableSubscribe;
-    }
-
-    registerErrorListener() {
-        // Invoke onError empApi method
-        onError((error) => {
-            console.log('Received error from server: ', JSON.stringify(error));
-            // Error contains the server-side error
-        });
-    }
 }
