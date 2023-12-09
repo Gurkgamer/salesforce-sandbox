2242ebc432d97880738ea231f216171bf38c94bd
Gurk <gurkgamer@gmail.com>
2023-12-09 07:10:42 +0100

    Dropbox y suscripcion

--- force-app/main/default/lwc/eventViewer/eventViewer.js
+++ force-app/main/default/lwc/eventViewer/eventViewer.js
@@ -1,27 +1,63 @@
-import { LightningElement,track } from 'lwc';
+import { LightningElement,wire, track } from 'lwc';
 import { subscribe, unsubscribe} from 'lightning/empApi';
+import getPlatformEventList from "@salesforce/apex/EventViewerController.getAvailablePlatformEvents";
 
 export default class EventViewer extends LightningElement
 {
     sfLogEvent = '/event/Log__e';
     sfLogEventSubscription = {};
     lastMessage = 'nada por ahora';
+    platformEventList = [];
+    dropboxValues = [];
+    currentSelectedPE;
 
     connectedCallback()
     {
-        this.subscribeToLogEvent();
+        //this.subscribeToLogEvent();
     }
 
-    subscribeToLogEvent()
+    handleSubscribe()
     {
-        this.sfLogEventSubscription = subscribe(this.sfLogEvent,-1,this.handleEvent.bind(this))
+        this.subscribeToLogEvent(this.currentSelectedPE);
+    }
+
+    disconnectedCallback() {
+        this.unsuscribeToLogEvent();
+    }
+
+    @wire(getPlatformEventList)
+    _platformEventList({ error, data })
+    {
+        if(data)
+        {
+            this.platformEventList = data;
+            this.dropboxValues = [];
+            for (const label of data) {
+                const option = {
+                    value: label,
+                    label: label
+                };
+
+                this.dropboxValues.push(option);
+            }
+            console.log(data);
+        }
+    }
+
+
+    subscribeToLogEvent(eventName)
+    {
+        const eventDescribe = '/event/'+eventName;
+        //this.sfLogEventSubscription = subscribe(this.sfLogEvent,-1,this.handleEvent.bind(this))
+        this.sfLogEventSubscription = subscribe(eventDescribe,-1,this.handleEvent.bind(this))
+
         .then(response =>
             {
                 console.log('Successfully subscribed to : ', JSON.stringify(response.channel));
             });
     }
 
-    unsuscribeToLogEvent()
+    unsubscribeToLogEvent()
     {
         unsubscribe(this.sfLogEventSubscription, response => {
             console.log('Successfully unsubscribed from : ', JSON.stringify(response.channel));
@@ -29,18 +65,54 @@ export default class EventViewer extends LightningElement
         }, onError);
     }
 
+    data = [];
+    columns = [];
+    tempColumns = [];
+    selected = [];
+    columnOptions = [];
+
     handleEvent(message)
     {
         try
         {
-            console.log('Received message: ', JSON.stringify(message));
-            // Handle the received event message
             this.lastMessage = message.data.payload.Message__c;
+
+            if(this.columns.length == 0)
+            {
+                const jsonData = JSON.parse(JSON.stringify(message));
+
+                const payloadKeys = Object.keys(jsonData.data.payload);
+
+                this.columns = payloadKeys.map((key) => ({
+                    label: key,
+                    fieldName: key,
+                    type: 'text',
+                }));
+
+                this.tempColumns = this.columns;
+
+
+                this.columnOptions = payloadKeys.map((columnName) => ({
+                    label: columnName,
+                    value: columnName
+                }));
+
+            }
+
+            this.data = [...this.data, JSON.parse(JSON.stringify(message)).data.payload];
+
         }
         catch(error)
         {
             console.log('ERROR: '+ error.message);
+            console.log('LINE: '+ error.lineNumber);
         }
+    }
+
+    handlePEChosseingChange(event)
+    {
+        this.currentSelectedPE = event.detail.value;
+        //currentSelectedPE = message.value;
     }
 
     refreshComponent()
@@ -48,4 +120,14 @@ export default class EventViewer extends LightningElement
         this.dispatchEvent(new RefreshEvent());
     }
 
+    get selected() {
+        return this.selected.length ? this.selected : 'none';
+    }
+
+    handleChange(e)
+    {
+        this.selected = e.detail.value;
+        const selectedColumns = this.selected.map(selected => this.tempColumns.find(column => column.fieldName === selected));
+        this.columns = selectedColumns;
+    }
 }
